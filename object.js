ba65a392ace06fef6ee6185886fecea18e5c4c7d
KanchonBauri <kanchonbruei1998@gmail.com>
2024-09-05 12:15:46 +0600

    key and value check

--- object.js
+++ object.js
@@ -1,16 +1,42 @@
 const person = {
-    name : "mehedy",
-    address : "Rajshahi",
+    name: "mehedy",
+    address: "Rajshahi",
 }
-console.log (person.address); // Rajshahi
-console.log (typeof person.address); // string
+// console.log (person.address);
+// output: Rajshahi
+
+// console.log (person["address"]);
+// output: Rajshahi
+
+// console.log (typeof person.address);
+// putput: string
 
 
 // contact : "017000000000" এটা person এর মধ্যে অ্যাড করার নিয়ম 
 person.contact = "0170000000"
-console.log(person); //output: { name: 'mehedy', address: 'Rajshahi', contact: '0170000000' }
+// console.log(person);
+//output: { name: 'mehedy', address: 'Rajshahi', contact: '0170000000' }
 
 
 // mehedy এর যায়গাতে kanchon নাম টা অ্যাড করবো
 person.name = "kanchon";
-console.log(person); //output: { name: 'kanchon', address: 'Rajshahi', contact: '0170000000' }
+// console.log(person);
+//output: { name: 'kanchon', address: 'Rajshahi', contact: '0170000000' }
+
+
+//delete 
+delete person.name;
+// console.log(person);
+//output:-  name : "mehedy" এটা delete হইয়ে যাবে
+
+
+// একটা Object এর ভিতরে কি কি key এবং property আছে ওই গুলা চেক করবো
+const key = Object.keys(person);
+// console.log(key);
+// output: [ 'address', 'contact' ]
+
+
+// একটা Object এর ভিতরে কি কি value আছে ওই গুলা চেক করবো
+const value = Object.values(person);
+// console.log(value);
+// output: [ 'Rajshahi', '0170000000' ]
