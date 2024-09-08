2be740f60db64c8d4345181f7595abe201d49efc
KanchonBauri <kanchonbruei1998@gmail.com>
2024-09-08 08:56:39 +0600

    shopingCart

--- shopingCart.js
+++ shopingCart.js
@@ -1,14 +1,18 @@
-const products =  [
-    { name: 'shampoo', price: 300, quantity:3 },
-    { name: 'chiruni', price: 100, quantity:2 },
-    { name: 'shirt', price: 700, quantity: 6},
-    { name: 'pant', price: 1200 , quantity:4},
+const products = [
+    { name: 'shampoo', price: 300, quantity: 3 },
+    { name: 'chiruni', price: 100, quantity: 2 },
+    { name: 'shirt', price: 700, quantity: 6 },
+    { name: 'pant', price: 1200, quantity: 4 },
 ]
 
-function cartTotal(products){
-    for(const product of products){
-        console.log(product)
+function cartTotal(products) {
+    let total = 0;
+    for (const product of products) {
+        // console.log(product)
+        const thisProductCost = product.price * product.quantity;
+        total = total + thisProductCost;
     }
+    return total;
 }
 
 const shoppingCart = cartTotal(products);
